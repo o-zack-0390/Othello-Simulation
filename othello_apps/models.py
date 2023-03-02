69c9e02896f962624dc6ddeffcb976e17cace521
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-03-02 20:48:36 +0900

    Update models.py

--- othello_apps/models.py
+++ othello_apps/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Koma(models.Model):
+    
+#   id   : 番号
+    id = models.IntegerField(primary_key=True)
+
+#   name : 駒名
+    name = models.CharField(max_length=100)
+
+#   hp   : 駒のHP
+    hp = models.IntegerField(blank=False, null=False)
+
+#   atk  : 駒の攻撃力
+    atk = models.IntegerField(blank=False, null=False)
+
+#   start_f_normal_s1  : (通常ダメージ, 固定, 最小値)
+    start_f_normal_s1  = models.IntegerField(blank=False, null=False)
+
+#   end_f_normal_s1    : (通常ダメージ, 固定, 最大値)
+    end_f_normal_s1    = models.IntegerField(blank=False, null=False)
+
+#   start_m_normal_s1  : (通常ダメージ, 倍率, 最小値)
+    start_m_normal_s1  = models.DecimalField(blank=False, null=False)
+
+#   upper_m_normal_s1  : (通常ダメージ, 倍率, 上昇値)
+    upper_m_normal_s1  = models.DecimalField(blank=False, null=False)
+
+#   end_m_normal_s1    : (通常ダメージ, 倍率, 最大値)
+    end_m_normal_s1    = models.DecimalField(blank=False, null=False)
+
+#   start_f_special_s1 : (特殊ダメージ, 固定, 最小値)
+    start_f_special_s1 = models.IntegerField(blank=False, null=False)
+
+#   end_f_special_s1   : (特殊ダメージ, 固定, 最大値)
+    end_f_special_s1   = models.IntegerField(blank=False, null=False)
+
+#   start_m_special_s1 : (特殊ダメージ, 倍率, 最小値)
+    start_m_special_s1 = models.DecimalField(blank=False, null=False)
+
+#   upper_m_special_s1 : (特殊ダメージ, 倍率, 上昇値)
+    upper_m_special_s1 = models.DecimalField(blank=False, null=False)
+
+#   end_m_special_s1   : (特殊ダメージ, 倍率, 最大値)
+    end_m_special_s1   = models.DecimalField(blank=False, null=False)
+
+#   active_turn_s1     : (スキル継続ターン数)
+    active_turn_s1     = models.PositiveSmallIntegerField(blank=False, null=False)
