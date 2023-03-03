2009b04a276bb8af153d87041e66abd84a004fd3
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-03-03 19:30:36 +0900

    Update models.py

--- othello_apps/models.py
+++ othello_apps/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Koma(models.Model):
+class Koma_Status(models.Model):
     
 #   id   : 番号
     id   = models.IntegerField(primary_key=True)
@@ -53,8 +53,11 @@ class Koma(models.Model):
 #   end_m_special_s1   : (特殊ダメージ, 倍率, 最大値)
     end_m_special_s1   = models.DecimalField(blank=False, null=False)
 
-#   upper_m_type       : 倍率上昇の形式 → 「演算方式」or「加算方式」 (固定ダメージの場合はnull)
-    upper_m_type       = models.CharField(blank=True, null=True)
+#   upper_m_type_s1    : 倍率上昇の形式 → 「演算方式」or「加算方式」 (固定ダメージの場合はnull)
+    upper_m_type_s1    = models.CharField(blank=True, null=True)
+
+#   target_type_s1     : 倍率上昇が「自分の駒数依存」 or 「相手の駒数依存」
+    target_type_s1     = models.CharField(blank=True, null=True)
 
 #   active_turn_s1     : (スキル継続ターン数)
     active_turn_s1     = models.PositiveSmallIntegerField(blank=False, null=False)
