ecad4b7f179b73caa2c64529deb34f800ab17fc2
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-03-06 10:58:09 +0900

    Update models.py

--- othello_apps/models.py
+++ othello_apps/models.py
@@ -3,17 +3,20 @@ from django.db import models
 # 駒のステータス
 class Status(models.Model):
     
-#   id   : 番号
-    id   = models.IntegerField(blank=False, null=False, primary_key=True)
+#   id     : 番号
+    id     = models.IntegerField(blank=False, null=False, primary_key=True)
 
-#   name : 駒名
-    name = models.CharField(blank=False, null=False, max_length=50)
+#   rarity : 「S」 or 「A」 or [B]
+    rarity = models.CharField(blank=False, null=False, max_length=5)
 
-#   hp   : 駒のHP
-    hp   = models.IntegerField(blank=False, null=False)
+#   name   : 駒名
+    name   = models.CharField(blank=False, null=False, max_length=50, primary_key=True)
 
-#   atk  : 駒の攻撃力
-    atk  = models.IntegerField(blank=False, null=False)
+#   hp     : 駒のHP
+    hp     = models.IntegerField(blank=False, null=False)
+
+#   atk    : 駒の攻撃力
+    atk    = models.IntegerField(blank=False, null=False)
 
     def __str__(self):
         return self.name
@@ -26,7 +29,7 @@ class Skill(models.Model):
     status             = models.ForeignKey(Status, on_delete=models.CASCADE)
 
 #   name               : 駒名
-    name               = models.CharField(blank=False, null=False, max_length=50)
+    name               = models.CharField(blank=False, null=False, max_length=50, primary_key=True)
 
 #   start_f_normal_s1  : (通常ダメージ, 固定, 最小値)
     start_f_normal_s1  = models.IntegerField(blank=True, null=True)
@@ -38,19 +41,19 @@ class Skill(models.Model):
     end_f_normal_s1    = models.IntegerField(blank=True, null=True)
 
 #   start_m_normal_s1  : (通常ダメージ, 倍率, 最小値)
-    start_m_normal_s1  = models.DecimalField(blank=True, null=True)
+    start_m_normal_s1  = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   upper_m_normal_s1  : (通常ダメージ, 倍率, 上昇値)
-    upper_m_normal_s1  = models.DecimalField(blank=True, null=True)
+    upper_m_normal_s1  = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   end_m_normal_s1    : (通常ダメージ, 倍率, 最大値)
-    end_m_normal_s1    = models.DecimalField(blank=True, null=True)
+    end_m_normal_s1    = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   is_shield_pass_s1  : 貫通スキルか判定
-    is_shield_pass_s1  = models.BooleanField(blank=True, null=True)
+    is_shield_pass_s1  = models.BooleanField()
 
 #   is_debuff_pass_s1  : デバフ無効スキルか判定
-    is_debuff_pass_s1  = models.BooleanField(blank=True, null=True)
+    is_debuff_pass_s1  = models.BooleanField()
 
 #   start_f_special_s1 : (特殊ダメージ, 固定, 最小値)
     start_f_special_s1 = models.IntegerField(blank=True, null=True)
@@ -62,19 +65,19 @@ class Skill(models.Model):
     end_f_special_s1   = models.IntegerField(blank=True, null=True)
 
 #   start_m_special_s1 : (特殊ダメージ, 倍率, 最小値)
-    start_m_special_s1 = models.DecimalField(blank=True, null=True)
+    start_m_special_s1 = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   upper_m_special_s1 : (特殊ダメージ, 倍率, 上昇値)
-    upper_m_special_s1 = models.DecimalField(blank=True, null=True)
+    upper_m_special_s1 = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   end_m_special_s1   : (特殊ダメージ, 倍率, 最大値)
-    end_m_special_s1   = models.DecimalField(blank=True, null=True)
+    end_m_special_s1   = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   barrier_s1         : バリア値
     barrier_s1         = models.IntegerField(blank=True, null=True)
 
 #   barrier_return_s1  : バリアの残ダメージ
-    barrier_return_s1  = models.DecimalField(blank=True, null=True)
+    barrier_return_s1  = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   barrier_type_s1    : バリアのタイプ → 「通常」 or 「特殊」
     barrier_type_s1    = models.CharField(blank=False, null=False, max_length=10)
@@ -84,6 +87,9 @@ class Skill(models.Model):
 
 #   target_type_s1     : 倍率上昇が「自分の駒数依存」 or 「相手の駒数依存」
     target_type_s1     = models.CharField(blank=True, null=True, max_length=10)
+
+#   summons_s1         : 召喚駒
+    summons_s1         = models.CharField(blank=True, null=True, max_length=50)
 
 #   active_turn_s1     : (スキル継続ターン数)
     active_turn_s1     = models.PositiveSmallIntegerField(blank=False, null=False)
@@ -99,7 +105,7 @@ class Combo_Skill(models.Model):
     status             = models.ForeignKey(Status, on_delete=models.CASCADE)
 
 #   name               : 駒名
-    name               = models.CharField(blank=False, null=False, max_length=50)
+    name               = models.CharField(blank=False, null=False, max_length=50, primary_key=True)
 
 #   start_f_normal_s2  : (通常ダメージ, 固定, 最小値)
     start_f_normal_s2  = models.IntegerField(blank=True, null=True)
@@ -111,22 +117,19 @@ class Combo_Skill(models.Model):
     end_f_normal_s2    = models.IntegerField(blank=True, null=True)
 
 #   start_m_normal_s2  : (通常ダメージ, 倍率, 最小値)
-    start_m_normal_s2  = models.DecimalField(blank=True, null=True)
+    start_m_normal_s2  = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   upper_m_normal_s2  : (通常ダメージ, 倍率, 上昇値)
-    upper_m_normal_s2  = models.DecimalField(blank=True, null=True)
+    upper_m_normal_s2  = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   end_m_normal_s2    : (通常ダメージ, 倍率, 最大値)
-    end_m_normal_s2    = models.DecimalField(blank=True, null=True)
+    end_m_normal_s2    = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   is_shield_pass_s2  : 貫通スキルか判定
-    is_shield_pass_s2  = models.BooleanField(blank=True, null=True)
+    is_shield_pass_s2  = models.BooleanField()
 
-#   is_shield_pass_s1  : 貫通スキルか判定
-    is_shield_pass_s2  = models.BooleanField(blank=True, null=True)
-
-#   is_debuff_pass_s1  : デバフ無効スキルか判定
-    is_debuff_pass_s2  = models.BooleanField(blank=True, null=True)
+#   is_debuff_pass_s2  : デバフ無効スキルか判定
+    is_debuff_pass_s2  = models.BooleanField()
 
 #   start_f_special_s2 : (特殊ダメージ, 固定, 最小値)
     start_f_special_s2 = models.IntegerField(blank=True, null=True)
@@ -138,19 +141,19 @@ class Combo_Skill(models.Model):
     end_f_special_s2   = models.IntegerField(blank=True, null=True)
 
 #   start_m_special_s2 : (特殊ダメージ, 倍率, 最小値)
-    start_m_special_s2 = models.DecimalField(blank=True, null=True)
+    start_m_special_s2 = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   upper_m_special_s2 : (特殊ダメージ, 倍率, 上昇値)
-    upper_m_special_s2 = models.DecimalField(blank=True, null=True)
+    upper_m_special_s2 = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   end_m_special_s2   : (特殊ダメージ, 倍率, 最大値)
-    end_m_special_s2   = models.DecimalField(blank=True, null=True)
+    end_m_special_s2   = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   barrier_s2         : バリア値
     barrier_s2         = models.IntegerField(blank=True, null=True)
 
 #   barrier_return_s2  : バリアの残ダメージ
-    barrier_return_s2  = models.DecimalField(blank=True, null=True)
+    barrier_return_s2  = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   barrier_type_s2    : バリアのタイプ → 「通常」 or 「特殊」
     barrier_type_s2    = models.CharField(blank=False, null=False, max_length=10)
@@ -160,6 +163,9 @@ class Combo_Skill(models.Model):
 
 #   target_type_s2     : 倍率上昇が「自分の駒数依存」 or 「相手の駒数依存」
     target_type_s2     = models.CharField(blank=True, null=True, max_length=10)
+
+#   summons_s2         : 召喚駒
+    summons_s2         = models.CharField(blank=True, null=True, max_length=50)
 
 #   active_turn_s2     : (スキル継続ターン数)
     active_turn_s2     = models.PositiveSmallIntegerField(blank=False, null=False)
@@ -172,22 +178,22 @@ class Combo_Skill(models.Model):
 class Shield(models.Model):
     
 #   id          : 番号
-    id          = models.IntegerField(blank=False, null=False, primary_key=True)
+    id          = models.IntegerField(blank=False, null=False)
 
 #   name        : 駒名
-    name        = models.CharField(blank=False, null=False, max_length=50)
+    name        = models.CharField(blank=False, null=False, max_length=50, primary_key=True)
 
 #   debuff      : 通常デバフ率
-    debuff      = models.IntegerField(blank=True, null=True)
+    debuff      = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   coercion    : 威圧
     coercion    = models.IntegerField(blank=True, null=True)
 
 #   normal_cut  : 通常カット率
-    normal_cut  = models.IntegerField(blank=True, null=True)
+    normal_cut  = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   special_cut : 特殊カット率
-    special_cut = models.IntegerField(blank=True, null=True)
+    special_cut = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
 
 #   cut_type    : 「表になっている間」 or 「ひっくり返されたとき」
     cut_type    = models.CharField(blank=False, null=False, max_length=10)
