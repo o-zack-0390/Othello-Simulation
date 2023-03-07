031318bae79ecfc25246e9f72e62c745bd8aa36c
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-03-07 20:56:34 +0900

    update

--- othello_apps/admin.py
+++ othello_apps/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Status
+from .models import Skill
+from .models import Combo_Skill
 
 # Register your models here.
+admin.site.register(Status)
+admin.site.register(Skill)
+admin.site.register(Combo_Skill)
